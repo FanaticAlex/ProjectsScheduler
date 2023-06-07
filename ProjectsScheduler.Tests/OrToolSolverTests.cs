c0b11293386f82be9d3204a7714403f3508518dd
FanaticAlex <fanatic8787@gmail.com>
2023-06-07 17:10:51 +0300

    Add example6 test

--- ProjectsScheduler.Tests/OrToolSolverTests.cs
+++ ProjectsScheduler.Tests/OrToolSolverTests.cs
@@ -7,15 +7,27 @@ namespace ProjectsScheduler.Tests
     public class OrToolSolverTests
     {
         [Fact]
-        public void Test_Example1()
+        public void Test_Example5()
         {
-            var data = File.ReadAllText("Example1.json");
+            var data = File.ReadAllText("Example5.json");
             var projectSet = JsonSerializer.Deserialize<ProjectsSet>(data);
             var solver = new ProjectSchedulerProblemSolver();
             var result = solver.Solve(projectSet);
 
             Assert.True(result.Success);
             Assert.Equal(15, result.OverallTime);
+        }
+
+        [Fact]
+        public void Test_Example6()
+        {
+            var data = File.ReadAllText("Example6.json");
+            var projectSet = JsonSerializer.Deserialize<ProjectsSet>(data);
+            var solver = new ProjectSchedulerProblemSolver();
+            var result = solver.Solve(projectSet);
+
+            Assert.True(result.Success);
+            Assert.Equal(6, result.OverallTime);
         }
     }
 }
